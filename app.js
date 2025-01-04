ac3c7daa1277caae22bf84d78190256f4a0a3b5e
SolArgentinoTV2025 <traplegionrecords2020@gmail.com>
2025-01-04 05:34:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Función para abrir el modal
 function openModal(modalId) {
     var modal = document.getElementById(modalId);
@@ -23,30 +22,4 @@ window.onclick = function(event) {
 
 function verPelicula(url) {
     window.location.href = url;
-=======
-// Función para abrir el modal
-function openModal(modalId) {
-    var modal = document.getElementById(modalId);
-    modal.style.display = "block";
-}
-
-// Función para cerrar el modal
-function closeModal(modalId) {
-    var modal = document.getElementById(modalId);
-    modal.style.display = "none";
-}
-
-// Cerrar el modal si se hace clic fuera de él
-window.onclick = function(event) {
-    var modals = document.querySelectorAll('.modal');
-    modals.forEach(function(modal) {
-        if (event.target === modal) {
-            modal.style.display = "none";
-        }
-    });
-}
-
-function verPelicula(url) {
-    window.location.href = url;
->>>>>>> 2df15a2257b04cefd38edadc59eea1c180474543
 }
