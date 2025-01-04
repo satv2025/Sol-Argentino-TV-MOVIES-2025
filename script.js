bb32cfa2c92a32ea810b08413bb3019b943e0cdc
skb10x <skboficicialvevo@gmail.com>
2025-01-03 23:35:22 -0300

    Subiendo la página base

---
+++ script.js
@@ -0,0 +1,5 @@
+function verPelicula(peliculaId) {
+    alert("Viendo: " + peliculaId);
+    // Aquí podrías redirigir al usuario a la página de streaming correspondiente
+    // por ejemplo, window.location.href = "ver_pelicula.html?id=" + peliculaId;
+}
