e670149a082c8a366691d06620f6d6e62b97c699
SolArgentinoTV2025 <traplegionrecords2020@gmail.com>
2025-02-05 16:55:21 -0300

    Modal

--- modal.js
+++ modal.js
@@ -1,3 +1,33 @@
+// Definir los datos de cada película
+const peliculas = {
+    matiponcepeli: {
+        year: "2022",
+        duration: "1h",
+        description: "Matías Ponce, creador de contenido en redes, se enfrenta a alienígenas y hackers tras la misteriosa suspensión de su canal de YouTube, o como él lo llama 'Yutun', desatando una hilarante y absurda aventura.",
+        createdBy: "<strong>Creado por:</strong> Matias Ponce",
+        cast: "<strong>Elenco:</strong> Matias Ponce, Santino Ponce, Indio Ponce, <a>más</a>",
+        title: "Acerca de Matias Ponce - La Película",
+        script: "<strong>Guión:</strong> Matias Ponce",
+        genres: "<strong>Géneros:</strong> Humor, humor absurdo, hackers, fama, grandes élites, paranoia, haters, YouTube",
+        titleType: "<strong>Este título es:</strong> Original, delirante",
+        ageRating: "<span class='age'>16+</span> adolescentes, adultos, lenguaje inapropiado, delirante | Apta para mayores de 16 años",
+        background: "maxresdefault.jpg",
+        link: "#"
+    },
+    cienporcientolucha: {
+        year: "2008-09",
+        duration: "1h 20m (por película)",
+        description: "Los productores detrás del famoso programa 100% Lucha decidieron hacerle películas.",
+        createdBy: "<strong>Creado por:</strong> Eduardo Husni",
+        cast: "<strong>Elenco:</strong> Daniel Garcilazo, Paulo Giardina, Gabriel Emanuelli, <a>más</a>",
+        title: "Acerca de 100%Lucha - Las Películas",
+        script: "<strong>Guión:</strong> CineAR",
+        genres: "<strong>Géneros:</strong> Humor, lucha libre, luchas clandestinas",
+        titleType: "<strong>Este título es:</strong> Intenso, Íntimo, Delirante, Divertido",
+        ageRating: "<span class='age'>12+</span> lenguaje inapropiado, daños físicos, humor absurdo",
+        background: "https://www.mediafire.com/convkey/4a6d/478xmus1d6v68eh9g.jpg?size_id=6",
+        link: "#"
+    },
 // Datos de las películas
 const peliculas = {
     app: {
