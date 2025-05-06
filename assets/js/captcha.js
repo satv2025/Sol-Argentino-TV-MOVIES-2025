39e5e636a695a9c53afbc48901e4f073da3211c6
satv2025 <traplegionrecords2020@gmail.com>
2025-05-06 19:08:14 -0300

    act

---
+++ assets/js/captcha.js
@@ -0,0 +1,19 @@
+lucide.createIcons();
+
+function checkCaptcha() {
+  const recaptchaResponse = grecaptcha.getResponse();
+  const turnstileResponse = document.querySelector('input[name="cf-turnstile-response"]')?.value;
+  const accessBtn = document.getElementById('access-btn');
+
+  if (recaptchaResponse && turnstileResponse) {
+    // Habilitar el botón
+    accessBtn.disabled = false;
+    accessBtn.classList.add('enabled');
+    accessBtn.onclick = () => {
+      window.location.href = "/inicio.html";
+    };
+  }
+}
+
+    // Inicializar íconos Lucide
+    lucide.createIcons();
